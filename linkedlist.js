25c26a56b0896222c8a0c4cbb414027099466bef
Louis <louis.dubois@epitech.eu>
2026-07-13 21:53:54 +0200

    ADD: methode prepend - tail

--- linkedlist.js
+++ linkedlist.js
@@ -10,6 +10,20 @@ class LinkedList {
         newNode.nextNode = this.headNode;
         this.headNode = newNode;
     }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.nextNode = this.headNode;
+        this.headNode = newNode;
+    }
+
+    tail() {
+        let node = this.headNode;
+        
+        while (node.nextNode !== null)
+            node = node.nextNode;
+        return node.value;
+    }
 }
 
 export { LinkedList };
--- script.test.js
+++ script.test.js
@@ -3,7 +3,17 @@ import { LinkedList  } from './linkedlist.js';
 
 const list = new LinkedList();
 
-test("Add new node", () => {
-    list.append("First");
+test("Prepend a node", () => {
+    list.prepend("First");
     expect(list.headNode.value).toBe("First");
 });
+
+test("Prepend second node", () => {
+    list.prepend("Second");
+    expect(list.headNode.value).toBe("Second");
+})
+
+test("Append Node and return his value", () => {
+    list.append("last");
+    expect(list.tail()).toBe("last");
+})
